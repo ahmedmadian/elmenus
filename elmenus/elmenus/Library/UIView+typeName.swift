2469f0d64522921d2bd87be8d31165ba43133634
AhmedMadian <ahmed.mostafa.madian@gmail.com>
2020-02-17 14:30:39 +0200

    add configuration for menu moodule components

---
+++ elmenus/elmenus/Library/UIView+typeName.swift
@@ -0,0 +1,15 @@
+//
+//  UIView+typeName.swift
+//  elmenus
+//
+//  Created by Ahmed Madian on 2/17/20.
+//  Copyright © 2020 Ahmed Madian. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    static var typeName: String {
+        return String(describing: self)
+    }
+}
--- elmenus/elmenus/Modules/Menu/MenuViewController.swift
+++ elmenus/elmenus/Modules/Menu/MenuViewController.swift
@@ -20,10 +20,36 @@ class MenuViewController: UIViewController, BindableType {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        congifTableView()
+        configureCollectionView()
     }
     
     func bindViewModel() {
     }
     
+    private func congifTableView() {
+        let itemNib = UINib(nibName: ItemCell.typeName, bundle: nil)
+        tableView.register(itemNib, forCellReuseIdentifier: ItemCell.typeName)
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 350
+    }
+    
+    private func registerCell() {
+        let tagCellNib = UINib(nibName: TagCell.typeName, bundle: nil)
+        collectionView.register(tagCellNib, forCellWithReuseIdentifier: TagCell.typeName)
+    }
+
+    private func configureCollectionView() {
+        registerCell()
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .horizontal
+        let itemWidth = collectionView.bounds.width/2 - 20
+        let itemHeight = collectionView.bounds.height
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemHeight)
+        flowLayout.minimumInteritemSpacing = 10
+        flowLayout.minimumLineSpacing = 10
+        flowLayout.sectionInset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+        collectionView.setCollectionViewLayout(flowLayout, animated: true)
+    }
 
 }
